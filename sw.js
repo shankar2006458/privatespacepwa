2d7281395d9b3afaf46305943450024f72b9c004
Shankar Prasad Pyke <bikramff54@gmail.com>
2026-03-19 20:44:33 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "love-app-v2";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
